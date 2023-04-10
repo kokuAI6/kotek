8ecaa2e6a392c807c75991686e9020b4a6e1b344
kokuAI6 <130328934+kokuAI6@users.noreply.github.com>
2023-04-10 14:36:17 +1000

    Update main.js

--- main.js
+++ main.js
@@ -21,6 +21,25 @@ function detectIntent(query) {
 const input = document.getElementById('bindu-button');
 const output = document.getElementById('output');
 
+const projectId = 'your-project-id';
+const sessionId = 'your-session-id';
+const languageCode = 'en';
+const ttsClient = new textToSpeech.TextToSpeechClient();
+
+async function synthesizeSpeech(text) {
+  const request = {
+    input: {text: text},
+    voice: {languageCode: 'en-US', ssmlGender: 'FEMALE'},
+    audioConfig: {audioEncoding: 'MP3'},
+  };
+
+  const [response] = await ttsClient.synthesizeSpeech(request);
+  return response.audioContent;
+}
+
+const input = document.getElementById('bind
+
+
 input.addEventListener('click', async function() {
   const query = 'Hello, world!'; // Replace with user input
   const response = await detectIntent(query);
